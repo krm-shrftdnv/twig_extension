b2ecfb0b57015dd235e9220974eff3f7d3984491
krm_shrftdnv <krm_shrftdnv@deck.lc>
2022-11-08 12:21:59 +0300

    fixing buggies

--- twig_extension/src/Controller/MainController.php
+++ twig_extension/src/Controller/MainController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     private static array $names = [];
 
     /**
-     * @Route("/{page}", methods = ['GET', 'POST'], name="spa", requirements = {"page"="^((?!api/)(?!static/)(?!icon.svg)).*$"})
+     * @Route("/{page}", methods = "[GET, POST]", name="spa", requirements = {"page"="^((?!api/)(?!static/)(?!icon.svg)).*$"})
      */
     public function index(): Response
     {
